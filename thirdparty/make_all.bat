1741e62c2d886a8077a8ba9aa39d1362cfe2da4d
xiaolanpingguo <xiaolanpingguo@163.com>
2023-05-24 18:16:00 +0800

    Update make_all.bat

--- thirdparty/make_all.bat
+++ thirdparty/make_all.bat
@@ -1,9 +1,44 @@
-@echo "=======begin build all thirdparty=========="
+@echo "=======begin build redis=========="
 
-start make_redis.bat
-start make_mongo-c.bat
-start make_protobuf.bat
+cd hiredis-1.1.0
+rmdir  /s /q .\win-build\
+rmdir /s /q  .\win-install\
+mkdir win-build
+cd win-build
+cmake .. -G "Visual Studio 17 2022" -DCMAKE_INSTALL_PREFIX=..\win-install
+cmake --build . --target install
+cd ../..
+
+@echo "=======finish build redis=========="
+
+
+@echo "=======begin build mongo-c=========="
+
+cd mongo-c-driver-1.23.3
+rmdir  /s /q .\win-build\
+rmdir /s /q  .\win-install\
+mkdir win-build
+cd win-build
+cmake .. -G "Visual Studio 17 2022" -DCMAKE_INSTALL_PREFIX=..\win-install -DENABLE_AUTOMATIC_INIT_AND_CLEANUP=OFF -DENABLE_EXTRA_ALIGNMENT=0
+cmake --build . --config RelWithDebInfo --target install
+cd ../..
+
+
+@echo "=======finish build mongo-c=========="
+
+
+@echo "=======begin build protobuf=========="
+
+cd protobuf-22.1
+rmdir  /s /q .\win-build\
+rmdir /s /q  .\win-install\
+mkdir win-build
+cd win-build
+cmake .. -G "Visual Studio 17 2022" -DCMAKE_INSTALL_PREFIX=..\win-install -Dprotobuf_MSVC_STATIC_RUNTIME=off
+cmake --build . --target install
+cd ../..
+
+
+@echo "=======finish build protobuf=========="
 
 pause
-
-@echo "=======finish build all thirdparty=========="
